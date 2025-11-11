b4417a80c101836f31b5e30cfacf0b0c71bd2244
Khiew Jian Bin <khiewbin@gmail.com>
2025-11-11 17:15:07 +0800

    shift

--- Assets/InputMouse.cs
+++ Assets/InputMouse.cs
@@ -1,32 +1,20 @@
 ﻿using UnityEngine;
-using UnityEditor;
 
-[ExecuteInEditMode]
-public class InputMouse : MonoBehaviour 
+public class InjectMouse : MonoBehaviour
 {
-	public void Update () 
-	{
-		GetComponent<Renderer>().sharedMaterial.SetVector("mouse", new Vector4(Input.mousePosition.x/Screen.width,Input.mousePosition.y/Screen.height,0,0));
-	}
-	void OnEnable()
-	{
-		SceneView.onSceneGUIDelegate += SceneGUI;
-	}
-	void OnDisable()
-	{
-		SceneView.onSceneGUIDelegate -= SceneGUI;
-	}
-	
-	void SceneGUI(SceneView sceneView)
-	{
-        if (!Application.isPlaying)
+    void Update()
+    {
+        Ray r = RectTransformUtility.ScreenPointToRay(Camera.main, Input.mousePosition);
+        if (Physics.Raycast(r, out var hitinfo))
         {
-            Vector3 mousePosition = Event.current.mousePosition;
-            //mousePosition.x -= Screen.width;
-            mousePosition.y -= Screen.height;
-            mousePosition.y = -mousePosition.y;
-            //Update () ;
-            GetComponent<Renderer>().sharedMaterial.SetVector("mouse", new Vector4(mousePosition.x / Screen.width, mousePosition.y / Screen.height, 0, 0));
+            var hitpos = hitinfo.textureCoord;
+
+            var renderer = GetComponent<Renderer>();
+            if (renderer == null) return;
+            var mat = renderer.sharedMaterial;
+            if (mat == null) return;
+
+            mat.SetVector("mouse", new Vector4(hitpos.x, hitpos.y, 0, 0));
         }
-	}
+    }
 }
